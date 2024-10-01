9a6a3bff46145ef3c84813d7ff31b93d02a5f5a4
Flávio Rocha <flavio-10_28@hotmail.com>
2024-10-01 17:18:52 -0300

    Criação do serviço de pesquisa do clime, criação da barra de pesquisa para busca do tempo

--- src/app/modules/weather/page/wheater-home/weather-home.component.ts
+++ src/app/modules/weather/page/wheater-home/weather-home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Subject, takeUntil } from 'rxjs';
+import { WeatherDatas } from 'src/app/models/interface/WeatherDatas';
+
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+
+  private readonly destroy$: Subject<void> = new Subject();
+
+  initialCityName: String = "São Paulo";
+  weatherData!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
+
+  constructor(private service: WeatherService) { }
+
+  ngOnInit(): void {
+    //this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: String): void {
+
+    this.service.getWeatherDatas(cityName)
+    .pipe(
+      takeUntil(this.destroy$)
+    ).subscribe({
+        next: (response) => {
+          response && (this.weatherData = response)
+          console.log(this.weatherData)
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      });
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }
